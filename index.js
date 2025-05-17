c29f18bad9d84249e5858908a642e1c7665234d7
ChickenStrips05 <luke.scarlatescu@gmail.com>
2025-05-17 08:25:16 -0400

    changed default port

--- index.js
+++ index.js
@@ -224,7 +224,7 @@ app.get("/*id", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
